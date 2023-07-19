d3041d19ab44bfe9c36fc2efe61266d16fdf3840
manaslaud <manaslaud@hotmail.com>
2023-07-19 19:03:31 +0530

    finalise changes

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -13,12 +13,13 @@ function ProfileCard() {
         <button className={styles.contactBtn}><a href='https://www.behance.net/manaslaud'>Behance</a></button>
         <button className={styles.githubBtn}><a href='https://github.com/manaslaud'>Github</a></button>
         </div>
+        <h1 className={styles.myProjText}> My Projects</h1>
         <div className={styles.skillsContainer}>
-            <span className={styles.skills}>UI/UX</span>
-            <span className={styles.skills}>Figma</span>
-            <span className={styles.skills}>React.js</span>
-            <span className={styles.skills}>Typescript</span>
-            <span className={styles.skills}>Frontend Web Development</span>
+             <span className={styles.skills}><a href='https://gravitas23-frontend-landing.vercel.app/'>graVITas 2023</a></span>
+             <span className={styles.skills}><a href='https://ffcs.csivit.com/'>FFCS Planner</a></span>
+            <span className={styles.skills}><a href='https://runaway-pink.vercel.app/'>Runaway</a></span>
+            <span className={styles.skills}><a href='https://github.com/manaslaud/spotify_charts'>Spotify DataApp</a></span>
+            <span className={styles.skills}><a href='https://share-app-manas.web.app/'>Share Place</a></span>
         </div>
         <AnimationText/>
         </div>
