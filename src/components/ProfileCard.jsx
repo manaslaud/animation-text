18c7590ab45520ea88f4ef2ebed7f2e5a53e0048
Manas Laud <119937760+manaslaud@users.noreply.github.com>
2023-08-09 00:14:38 +0530

    small changes

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -18,7 +18,7 @@ function ProfileCard() {
             <span className={styles.skills}><a href='https://3dverse.vercel.app/'> 3dverse</a></span>
             <span className={styles.skills}><a href='https://share-app-manas.web.app/'> Share Place App</a></span>
             <span className={styles.skills}><a href='https://ffcs.csivit.com/'>FFCS Planner</a></span>
-            <span className={styles.skills}>Frontend Web Development</span>
+            <span className={styles.skills}><a href='https://quiz-frontend-zeta.vercel.app/'>CSI Quiz</a></span>
         </div>
         <AnimationText/>
         </div>
